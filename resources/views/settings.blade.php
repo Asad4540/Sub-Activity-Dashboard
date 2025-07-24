c9d84344e18c0ecf7caa42e46cd0952f788277d0
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2025-07-24 19:16:41 +0530

    created settings page tabs

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <center>
-        <h3>Settings page</h3>
-    </center>
+
+    <div class="px-5 mt-4">
+        <div class="tabs-card d-flex">
+            <div class="nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <button class="nav-link active" id="profile-tab" data-bs-toggle="pill" data-bs-target="#profile"
+                    type="button" role="tab">Profile Information</button>
+                <button class="nav-link" id="security-tab" data-bs-toggle="pill" data-bs-target="#security" type="button"
+                    role="tab">Security & Password</button>
+                <button class="nav-link" id="notification-tab" data-bs-toggle="pill" data-bs-target="#notification"
+                    type="button" role="tab">Notification Preferences</button>
+                <button class="nav-link" id="devices-tab" data-bs-toggle="pill" data-bs-target="#devices" type="button"
+                    role="tab">Devices Info</button>
+            </div>
+
+            <div class="tab-content tab-wrapper" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="profile" role="tabpanel">Your profile information goes here.
+                </div>
+                <div class="tab-pane fade" id="security" role="tabpanel">Change password and security settings here.</div>
+                <div class="tab-pane fade" id="notification" role="tabpanel">Manage notifications and alerts here.</div>
+                <div class="tab-pane fade" id="devices" role="tabpanel">See and manage your logged-in devices here.</div>
+            </div>
+        </div>
+    </div>
 @endsection
