f38cd9bf2294a63a66a88c4ab3947c0cd3540322
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2025-09-02 19:33:01 +0530

    working on creating subscriber profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,14 +11,14 @@
 
         <div class="mt-2">
             <div class="row">
-                <div class="col-lg-8">
+                <div class="col-lg-8 col-md-12">
                     <div class="card mt-4">
                         <p class="settings-title p-4">
                             Subscriber Profile
                         </p>
                         <div class="d-flex justify-content-around">
                             <div>
-                                <img src="{{ asset('assets/images/admin.png') }}" alt="" class="mb-2">
+                                <img src="{{ asset('assets/images/admin.png') }}" alt="" class="mb-2 img-fluid">
                                 <p class="input-text text-center m-0 p-0">Alexa Fein</p>
                                 <p class="settings-sub-title text-center">Admin, Vgen</p>
                             </div>
@@ -53,21 +53,101 @@
                         <p class="settings-title p-4">
                             Content Interaction Summary 
                         </p>
+                        <div class="row d-flex justify-content-around mb-5">
+                            <div class="col-lg-3 d-flex gap-3 px-3 py-2 content-interaction-box"
+                                style="background:#E6FFEFCF">
+                                <div>
+                                    <img src="{{ asset('assets/images/editgreen.png') }}" class="img-fluid" alt="">
+                                </div>
+                                <div>
+                                    <span class="input-text">Articals Read</span> <br>
+                                    <span class="settings-title">52</span>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 d-flex gap-3 px-3 py-2 content-interaction-box" style="background:#F9EBED">
+                                <div>
+                                    <img src="{{ asset('assets/images/clock.png') }}" class="img-fluid" alt="">
+                                </div>
+                                <div>
+                                    <span class="input-text">Total Time Spend</span> <br>
+                                    <span class="settings-title">5h 2min</span>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 d-flex gap-3 px-3 py-2 content-interaction-box" style="background:#F8F4FC">
+                                <div>
+                                    <img src="{{ asset('assets/images/note.png') }}" class="img-fluid" alt="">
+                                </div>
+                                <div>
+                                    <span class="input-text">Whitepapers</span> <br>
+                                    <span class="settings-title">82</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row d-flex justify-content-around">
+                            <div class="col-lg-5 h-100" style="background:#F4F5F9">
+                                <p class="input-text ">Top Categories engaged</p>
+                            </div>
+                            <div class="col-lg-5 h-100" style="background:#F4F5F9">
+                                <p class="input-text">Content Type Interaction</p>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <div class="card  mt-4">
+                    <div class="row gap-4">
+                        <div class="col-lg card  mt-4">
                             <p class="settings-title p-4">
                                 Device & Login Info
                             </p>
+                            <div class="row px-3 d-flex justify-content-around gap-3">
+                                <div class="col-lg-5 d-flex border border-1 rounded p-2 gap-2">
+                                    <div>
+                                        <img src="{{ asset('assets/images/note.png') }}" class="img-fluid" alt="">
+                                    </div>
+                                    <div>
+                                        <span class="input-text">Whitepapers</span> <br>
+                                        <span class="settings-title">82</span>
+                                    </div>
+                                </div>
+                                <div class="col-lg-5 d-flex border border-1 rounded p-2 gap-2">
+                                    <div>
+                                        <img src="{{ asset('assets/images/note.png') }}" class="img-fluid" alt="">
+                                    </div>
+                                    <div>
+                                        <span class="input-text">Whitepapers</span> <br>
+                                        <span class="settings-title">82</span>
+                                    </div>
+                                </div>
+                                <div class="col-lg-5 d-flex border border-1 rounded p-2 gap-2">
+                                    <div>
+                                        <img src="{{ asset('assets/images/note.png') }}" class="img-fluid" alt="">
+                                    </div>
+                                    <div>
+                                        <span class="input-text">Whitepapers</span> <br>
+                                        <span class="settings-title">82</span>
+                                    </div>
+                                </div>
+                                <div class="col-lg-5 d-flex border border-1 rounded p-2 gap-2">
+                                    <div>
+                                        <img src="{{ asset('assets/images/note.png') }}" class="img-fluid" alt="">
+                                    </div>
+                                    <div>
+                                        <span class="input-text">Whitepapers</span> <br>
+                                        <span class="settings-title">82</span>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
-                        <div class="card  mt-4">
+                        <div class="col-lg card  mt-4">
                             <p class="settings-title p-4">
                                 Engagement Score
                             </p>
+                            <div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-lg-4 col-md-12">
                     <div class="card  mt-4">
                         <p class="settings-title p-4">
                             Recently Viewed
